ea4a4c4c5a029349446c46256e5ff2f582208507
wanghanxi0701 <3612384643@qq.com>
2026-08-12 19:23:48 +0800

    complete 10

--- exercises/10_trivial/main.cpp
+++ exercises/10_trivial/main.cpp
@@ -10,7 +10,7 @@ struct FibonacciCache {
 // TODO: 实现正确的缓存优化斐波那契计算
 static unsigned long long fibonacci(FibonacciCache &cache, int i) {
     for (; cache.cached <= i; ++cache.cached) {
-        cache.cache[cached] = cache.cache[cached - 1] + cache.cache[cached - 2];
+        cache.cache[cache.cached] = cache.cache[cache.cached - 1] + cache.cache[cache.cached - 2];
     }
     return cache.cache[i];
 }
