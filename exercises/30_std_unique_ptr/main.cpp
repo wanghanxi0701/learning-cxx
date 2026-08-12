bad543b3bacf81e855c0a3eab2f5c8b9ad347e6a
wanghanxi0701 <3612384643@qq.com>
2026-08-12 22:30:16 +0800

    30

--- exercises/30_std_unique_ptr/main.cpp
+++ exercises/30_std_unique_ptr/main.cpp
@@ -54,7 +54,7 @@ int main(int argc, char **argv) {
          //TODO: 分析 problems[1] 中资源的生命周期，将记录填入 `std::vector`
         // NOTICE: 此题结果依赖对象析构逻辑，平台相关，提交时以 CI 实际运行平台为准
         {"ffr", "d"},
-        {"d", "d"},
+        {"r", "d", "d"},
     };
 
     // ---- 不要修改以下代码 ----
